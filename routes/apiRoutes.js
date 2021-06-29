dd531a19cc52183168c092d900dc94944720bcb9
Chris Beard <cbeard@hnoj.org>
2021-06-29 16:58:22 -0500

    changed db folder location

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,16 +4,16 @@ const { v4: uuidv4 } = require('uuid');
 module.exports = (app) => {
 
     app.get('/api/notes', (req, res) => {
-        const notesInput = JSON.parse(fs.readFileSync('./db/db.json', "utf-8"));
+        const notesInput = JSON.parse(fs.readFileSync(`${__dirname}/../db/db.json`, "utf-8"));
         res.json(notesInput);
     });
 
     app.post("/api/notes", (res, req) => {
-        const notesInput=JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+        const notesInput=JSON.parse(fs.readFileSync(`${__dirname}/../db/db.json`, 'utf-8'));
         const newNote = req.body;
         newNote.id = uuidv4();
         notesInput.push(newNote);
-        fs.writeFileSync('./db/db.json', JSON.stringify(notesInput));
+        fs.writeFileSync(`${__dirname}/../db/db.json`, JSON.stringify(notesInput));
         res.json(newNote);
     });
 
