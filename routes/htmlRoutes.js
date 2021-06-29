7394c00a8879abeb0c0d65a80dbced2c55ff48eb
Chris Beard <cbeard@hnoj.org>
2021-06-29 16:35:15 -0500

    debugging again

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,12 +7,8 @@ module.exports = (app) => {
     });
 
 
-    /*  app.get("/", (req, res) => {
-         res.sendFile(path.join(__dirname, "../public/index.html"))
-     }); */
-
-
-    app.get("*", (req, res) => {
+    app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"))
     });
+
 };
