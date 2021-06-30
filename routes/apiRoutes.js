fea2cf215452e4fe21809e5c17ee7bd07b15e8c3
Chris Beard <cbeard@hnoj.org>
2021-06-30 14:31:33 -0500

    added delete button function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,4 +17,13 @@ module.exports = (app) => {
         res.json(newNote);
     });
 
+    app.delete('/api/notes/:id', (req, res) => {
+        let data = JSON.parse(fs.readFileSync('db/db.json', 'utf-8'));
+        let noteId = req.params.id;
+        let newArray = data.filter(note => note.id !== noteId);
+        fs.writeFileSync('db/db.json', JSON.stringify(newArray), (err) => {
+            if (err) throw err
+        });
+        res.json(newArray);
+    });
 };
